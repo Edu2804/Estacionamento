463f76d56ebb5e35f6ef4b90221fad6ac2e12464
Edu2804 <85701002+Edu2804@users.noreply.github.com>
2024-05-01 20:43:35 -0400

    Primeiro commit

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -3,12 +3,17 @@ package com.example.application.views.helloworld;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
@@ -17,19 +22,26 @@ public class HelloWorldView extends HorizontalLayout {
 
     private TextField name;
     private Button sayHello;
+    private ComboBox idade;
+
+    List<Integer> listaDeInteiros = new ArrayList<>();
+
 
     public HelloWorldView() {
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
+        idade = new ComboBox("Escolha sua idade");
+        idade.setItems(1,2,3,4,5,6,7,8,9,10);
         sayHello.addClickListener(e -> {
             Notification.show("Hello " + name.getValue());
         });
         sayHello.addClickShortcut(Key.ENTER);
 
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+       // setVerticalComponentAlignment(Alignment.END, name, idade,sayHello);
+        VerticalLayout vl = new VerticalLayout(name, sayHello, idade);
 
-        add(name, sayHello);
+        add(vl);
     }
 
 }
